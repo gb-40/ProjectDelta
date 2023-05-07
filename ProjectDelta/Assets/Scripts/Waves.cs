6ab62e7b8942754d8c9b25a3ed151cdb2246f0f3
gb-40 <yunggebe0k@gmail.com>
2023-05-06 23:30:02 -0500

    fixed waves

--- ProjectDelta/Assets/Scripts/Waves.cs
+++ ProjectDelta/Assets/Scripts/Waves.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Waves : MonoBehaviour
 {
-    public EnemySpawner enemySpawner, enemySpawner1;
+    public EnemySpawner enemySpawner, enemySpawner1, enemySpawner2, enemySpawner3;
 
     // Update is called once per frame
    void Update () {
       if(GameObject.FindGameObjectsWithTag("Enemy").Length <= 0){
          enemySpawner.SpawnMoreEnemies();
          enemySpawner1.SpawnMoreEnemies();
+         enemySpawner2.SpawnMoreEnemies();
+         enemySpawner3.SpawnMoreEnemies();
       }
    }
 }
